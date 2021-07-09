807edca8b804d2f1d6dd7469bc956a5194cd42a9
Kyle Nance <nancekyle15@gmail.com>
2021-07-08 22:24:30 -0400

    initial push

---
+++ scheduler.js
@@ -0,0 +1,10 @@
+const m = moment();
+console.log(m.toString());
+const currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+
+
+const eventMoment = moment().format('MMMM Do YYYY, h:mm:ss a');
+console.log(eventMoment)
+
+//let currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
